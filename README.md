a27e15acb61045c21786d20793f4acb4fb13a111
RomanKalsin <karen52rus@gmail.com>
2021-09-27 22:04:14 +0300

    HOME: add asciinema to readme.md, edit grain-even

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
 ### Code climate statys Maintainability
 <a href="https://codeclimate.com/github/RomanKalsin/python-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/b9ab0f3cc1aaf9149a0a/maintainability" /></a>
+
+### Example
+[![asciicast](https://asciinema.org/a/438405.svg)](https://asciinema.org/a/438405)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,18 +3,18 @@
 
 from brain_games.cli import welcome_user
 from random import randint
-import prompt 
+import prompt
 
 
 def main():
     print("Welcome to the Brain Games!")
     name = welcome_user()
     brain_even(name)
-    
+
 
 def brain_even(name):
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 0 
+    i = 0
     while i < 3:
         num = randint(1, 100)
         print("Question: {}".format(num))
@@ -22,10 +22,10 @@ def brain_even(name):
         ansver = prompt.string("Your answer: ")
         correct = "yes" if num % 2 == 0 else "no"
         if correct != ansver.lower():
-            print("'{}' is wrong answer ;(. Correct answer was '{}'. \n Let's try again, {}!".format(ansver, correct, name))
+            print("'{}' is wrong answer ;(. Correct answer was '{}'. "
+                  "\n Let's try again, {}!".format(ansver, correct, name))
             break
         print("Correct!")
         i += 1
     else:
         print("Congratulations, {}!".format(name))
-        
