c2a9bf920c5f31a0daeb0860104f9e576d10a5ad
RomanKalsin <karen52rus@gmail.com>
2021-09-27 17:00:20 +0300

    WORK: add brain-even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env/ python3
+
+
+from brain_games.cli import welcome_user
+from random import randint
+import prompt 
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = welcome_user()
+    brain_even(name)
+    
+
+def brain_even(name):
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    i = 0 
+    while i < 3:
+        num = randint(1, 100)
+        print("Question: {}".format(num))
+        correct = ""
+        ansver = prompt.string("Your answer: ")
+        correct = "yes" if num % 2 == 0 else "no"
+        if correct != ansver.lower():
+            print("'{}' is wrong answer ;(. Correct answer was '{}'. \n Let's try again, {}!".format(ansver, correct, name))
+            break
+        print("Correct!")
+        i += 1
+    else:
+        print("Congratulations, {}!".format(name))
+        
